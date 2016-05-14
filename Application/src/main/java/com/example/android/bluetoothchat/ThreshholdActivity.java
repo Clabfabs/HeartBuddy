5b4ef2bba4083c895b5bd31c0c65c719c32f6344
subash <luitel07@gmail.com>
2016-05-14 14:24:44 +0300

    Added threshhold class to adjust the threshhold of person and to send the adjusted value to arduino.

--- Application/src/main/java/com/example/android/bluetoothchat/ThreshholdActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/ThreshholdActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.common.activities.SampleActivityBase;
 
@@ -26,13 +27,12 @@ public class ThreshholdActivity extends SampleActivityBase {
      */
     private BluetoothChatService mChatService = null;
 
+    public byte min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.threshhold_setup);
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
         Button saveButton = (Button) findViewById(R.id.button_save);
@@ -45,14 +45,23 @@ public class ThreshholdActivity extends SampleActivityBase {
                 TextView Maximum_heart_rateTextview = (TextView) findViewById(R.id.set_max);
 
                 String Minimum_heart_rate = Minimum_heart_rateTextview.getText().toString();
+                String Minimum = "Min: " + Minimum_heart_rate + "#";
+                byte[] min = Minimum.getBytes();
+
                 String Maximum_heart_rate = Maximum_heart_rateTextview.getText().toString();
+                String Maximum = "Max: " + Maximum_heart_rate + "#";
+                byte[] max = Maximum.getBytes();
+
+                mChatService.write(min);
+                mChatService.write(max);
+
+                Toast toast = Toast.makeText(getApplicationContext(), "sent to Arduino", Toast.LENGTH_SHORT);
+                toast.show();
 
                 startActivity(new Intent(ThreshholdActivity.this, MainActivity.class));
 
             }
         });
-
     }
-
 }
 
