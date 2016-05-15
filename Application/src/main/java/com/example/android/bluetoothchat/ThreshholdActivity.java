d9e8178209ebcefb2c3773f8e41048d6f0cbfc14
Fabio <islerfab@gmail.com>
2016-05-15 10:33:51 +0300

    adjusted threshhold activity, but ultimately, the bluetooth doesn't get through yet

--- Application/src/main/java/com/example/android/bluetoothchat/ThreshholdActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/ThreshholdActivity.java
@@ -3,9 +3,12 @@ package com.example.android.bluetoothchat;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.common.activities.SampleActivityBase;
 
@@ -15,6 +18,10 @@ import com.example.android.common.activities.SampleActivityBase;
 public class ThreshholdActivity extends SampleActivityBase {
 
     public static final String TAG = "ThreshholdActivity";
+
+
+    private static final int REQUEST_ENABLE_BT = 3;
+
 
     /**
      * Local Bluetooth adapter
@@ -26,7 +33,6 @@ public class ThreshholdActivity extends SampleActivityBase {
      */
     private BluetoothChatService mChatService;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,25 +40,77 @@ public class ThreshholdActivity extends SampleActivityBase {
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        final Button saveButton = (Button) findViewById(R.id.save_threshhold);
 
-        Button saveButton = (Button) findViewById(R.id.save_threshhold);
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            mChatService = new BluetoothChatService(ThreshholdActivity.this, mHandler);
+        }
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                TextView set_min = (TextView) findViewById(R.id.set_min);
-                TextView set_max = (TextView) findViewById(R.id.set_max);
+                TextView min_text = (TextView) findViewById(R.id.set_min);
+                TextView max_text = (TextView) findViewById(R.id.set_max);
 
-                String min = set_min.getText().toString();
-                String max = set_max.getText().toString();
+                String min_string = min_text.getText().toString();
+                String min_msg = "L:" + min_string + "#"; // For "lower threshhold"
+                byte[] min = min_msg.getBytes();
+
+                String max_string = max_text.getText().toString();
+                String max_msg = "U: " + max_string + "#"; // For "upper threshhold"
+                byte[] max = max_msg.getBytes();
+
+                mChatService.write(min);
+                mChatService.write(max);
+
+                Toast toast = Toast.makeText(getApplicationContext(), "sent to Arduino", Toast.LENGTH_SHORT);
+                toast.show();
 
                 startActivity(new Intent(ThreshholdActivity.this, MainActivity.class));
 
             }
         });
-
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
+    /**
+     * The Handler that sends information with the BluetoothChatService
+     */
+    public final Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == Constants.MESSAGE_WRITE) {
+
+                // Do something with the sent message
+
+
+                byte[] writeBuf = (byte[]) msg.obj;
+                // construct a string from the buffer
+                String writeMessage = new String(writeBuf);
+                // mConversationArrayAdapter.add("Me:  " + writeMessage);
+            }
+        }
+    };
 }
 
